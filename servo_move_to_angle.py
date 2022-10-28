2295cf8340b92c13b1f74be34d3b60cd9078e675
SamsonGoodenough <goodenough.samson@gmail.com>
2022-10-28 04:22:03 +0100

    Finished lab 6b

--- servo_move_to_angle.py
+++ servo_move_to_angle.py
@@ -3,25 +3,23 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 
-THEORETICAL_LEFT = 1.0/20.0 * 100 # by calculation (1ms / 20ms * 100)
-THEORETICAL_RIGHT = 2.0/20.0 * 100 # by calculation (2ms / 20ms * 100)
 LEFT = 0.5/20.0 * 100 # by testing (0.5ms)
 RIGHT = 2.5/20.0 * 100 # by testing (2.5ms)
 MIDDLE = 1.5/20.0 * 100  # by calculation (1.5ms / 20ms * 100)
-
 DEG = (RIGHT - LEFT) / 190.0 # assume range of server is ~190deg
-current_angle = LEFT
-increment_angle = DEG*5
+
 p = GPIO.PWM(18, 50)  # channel=18 frequency=50Hz
 p.start(MIDDLE)
 time.sleep(1)
 try:
-    while current_angle < RIGHT:
-            p.ChangeDutyCycle(current_angle)
-            time.sleep(0.3)
-            current_angle += increment_angle
+  while True:
+    print("What angle do you want the servo?")
+    degrees = int(input())
+    angle = LEFT + DEG*degrees
+    p.ChangeDutyCycle(angle)
+    time.sleep(1)
 except KeyboardInterrupt:
-    pass
+  pass
 
 time.sleep(1)
 p.ChangeDutyCycle(MIDDLE)
